516bf9db735cf3b9b1dee888f54fe0fa31f9a8e7
jeriko3323 <jerikocoz@gmail.com>
2024-11-11 17:17:35 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       fontFamily: {
         'merriweather': ['Merriweather', 'serif'],
       }, dropShadow: {
-        'shad': '5px 5px 3px rgba(249, 150, 135, 100.3)',
+        'shad': '5px 5px 3px rgba(249, 150, 135, 100.3)'
       },
     },
   },
